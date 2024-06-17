942c2da7d26de03d7d20562a41cd28a155f7cd69
dosleam <9400@holbertonstudents.com>
2024-06-17 12:58:53 +0000

    C command

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -6,6 +6,7 @@
 int main(void)
 {
 	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -27,3 +28,4 @@ int main(void)
 			printf("and is less than 6 and not 0\n");
 		}
 	return (0);
+}
