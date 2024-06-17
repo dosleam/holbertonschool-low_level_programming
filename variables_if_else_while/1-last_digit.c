33276035e3954cb4c9ae3576290866cae64d8960
dosleam <9400@holbertonstudents.com>
2024-06-17 13:10:57 +0000

    C command

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -23,15 +23,15 @@ int main(void)
 		
 		if (last_digit > 5)
 		{
-			printf("and is greather than 5\n");
+			printf("%d and is greather than 5\n");
 		}
 		else if (last_digit == 0)
 		{
-			printf("and is 0\n");
+			printf("%d and is 0\n");
 		}
 		else
 		{
-			printf("and is less than 6 and not 0\n");
+			printf("%d and is less than 6 and not 0\n");
 		}
 	return (0);
 }
