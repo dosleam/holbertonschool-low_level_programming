c713f4cb84635e0482d08cb23d5b03e6f7379885
dosleam <9400@holbertonstudents.com>
2024-06-17 10:48:03 +0000

    C program

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,4 @@
 #include <stdio.h>
-
-/**
- * main - Prints \"Programming is like building a multilingual puzzle
- *
- * Return: 0
- */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
