3a19ceda5ae9e0751ac85b4bc9e903764b7236c3
dosleam <9400@holbertonstudents.com>
2024-06-18 09:19:24 +0000

    C command

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all possible combinations single numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+
+		putchar(num);
+	if (num < '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
+
+}
+	putchar('\n');
+
+	return (0);
+}
