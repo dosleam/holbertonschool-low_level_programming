c2e0e20fa8d2851dc512b7ed68b87b4be66ce37f
dosleam <9400@holbertonstudents.com>
2024-06-18 07:39:57 +0000

    C command

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Ecris l'alphabet en minuscule
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		if (alpha != 'e' && alpha != 'q')
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
