6c6e19f463da853f99c6b8c4ae4ddd06735a321b
dosleam <9400holbertonstudents@gmail.com>
2024-06-19 12:32:51 +0000

    C command

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,23 +2,25 @@
 
 /**
  * print_sign - prints the sign of a number
- * @n: the character to be checked
- * Return: 1 if is greater than zero, -1 if is less than zero, 0 if is zero
+ * @n: the number as be checked
+ * Return: 1 if n > 0, -1 if n < 0, 0 if n = 0
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		_putchar('+');
-	return (1);
-}
-{
+		return (1);
+	}
 	else if (n < 0)
+	{
 		_putchar('-');
-	return (-1);
-}
-{
+		return (-1);
+	}
 	else
+	{
 		_putchar('0');
-	return (0);
+		return (0);
+	}
 }
