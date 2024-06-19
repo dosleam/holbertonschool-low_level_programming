dbed42e99e2aae47d623100dfe5649e36b103446
dosleam <9400holbertonstudents@gmail.com>
2024-06-19 10:07:11 +0000

    C command

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,24 @@
  * Return: 0
  */
 
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
 		char alphabet = 'a';
 
 	while (alphabet <= 'z')
 	{
-		_putchar(alphabet * 10);
+		_putchar(alphabet);
 		alphabet++;
 	}
 
 	_putchar('\n');
 	}
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	while (count < 10) 
+	{
+		print_alphabet();
+		count++;
+	}
+}
