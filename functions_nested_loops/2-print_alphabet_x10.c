c3e8b19426e9deb4fa8fbbbf52d7f569f65cf0bc
dosleam <9400holbertonstudents@gmail.com>
2024-06-19 10:40:24 +0000

    C commnd

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,23 +2,9 @@
 
 /**
  * main - check the code
- * print_alphabet - Imprime l'alphabet en minuscules suivi d'une nouvelle ligne
  * print_alphabet_x10 - Imprime 10 fois l'alphabet en minuscules
  * Return: 0
  */
-
-void print_alphabet(void)
-{
-		char alphabet = 'a';
-
-	while (alphabet <= 'z')
-	{
-		_putchar(alphabet);
-		alphabet++;
-	}
-
-	_putchar('\n');
-	}
 
 void print_alphabet_x10(void)
 {
