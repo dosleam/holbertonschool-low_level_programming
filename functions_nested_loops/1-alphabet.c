38e10c807e01a5804083f30d7864ebb085844451
dosleam <9400holbertonstudents@gmail.com>
2024-06-19 09:07:23 +0000

    C command

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,12 +6,17 @@
  * Return: 0
  */
 
-int main(vid)
+void print_alphabet(void)
 {
-	for (char c = 'a' ; c <= 'z' ; c++)
-{	 _putchar(c);
-}
-	_putchar(\n);
+	char alpha = 'a';
 
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
 	return (0);
 }
+
+
