5cf8ad710ce152742b214d798a785eb5310c3c17
dosleam <9400holbertonstudents@gmail.com>
2024-06-19 07:25:02 +0000

    C command

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,1 +1,16 @@
-i
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print with printf
+ *
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,1 +1,10 @@
-i
+#include "main.h"
+
+/**
+ *
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void);
