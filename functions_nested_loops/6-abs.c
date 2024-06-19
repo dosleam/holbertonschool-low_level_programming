62e19c3df438419052d1fb1bf6a7d015fc5394d8
dosleam <9400holbertonstudents@gmail.com>
2024-06-19 12:45:03 +0000

    C command

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @r: the integer whose absolute value is required
+ * Return: 0
+ */
+
+int _abs(int)
+{
+	int r;
+	{
+		if (r < 0)
+		
+			printf("%d\n", r);
+		return (1);
+	}
+}
