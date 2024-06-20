f066f94d9fed29da130abae61e06d69330e976ff
dosleam <9400@holbertonstudents@gmail.com>
2024-06-20 15:46:06 +0000

    C command

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,2 +1,27 @@
-i
+#include "main.h"
+#include <stdio.h>
 
+/**
+ * print_to_98 - print all natural numbers from n to 98
+ * @n: number check
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	while (n != '98')
+	{
+		if (n < 98)
+		{
+			_putchar("%d, ", n);
+			n--;
+		}
+		else
+		{
+			_putchar("%d, ", n);
+			n++;
+		}
+	}
+	_putchar(98)
+	_putchar("\n");
+}	
