da88114bdca47b6ab4fd8ca6dcf8dabfe7a6a5c2
dosleam <9400@holbertonstudents@gmail.com>
2024-06-20 13:57:57 +0000

    C command

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: number 1
+ * @b: number 2
+ * Return: always 0
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
+
