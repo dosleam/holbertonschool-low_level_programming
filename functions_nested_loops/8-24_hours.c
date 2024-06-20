110a197ef8b58c753c07f71d98aa529b3ec48b48
dosleam <9400@holbertonstudents@gmail.com>
2024-06-20 11:44:36 +0000

    C command

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -23,11 +23,11 @@ void jack_bauer(void)
 				{
 					for (m2 = 0; m2 <= 9; m2++)
 					{
-			_putchar(h1 + '0');
-			_putchar(h2 + '0');
+			_putchar(h1);
+			_putchar(h2);
 			_putchar(':');
-			_putchar(m1 + '0');
-			_putchar(m2 + '0');
+			_putchar(m1);
+			_putchar(m2);
 			_putchar('\n');
 					}
 				}
