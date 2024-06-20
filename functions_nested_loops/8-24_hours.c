ed91ba65357c3f18e2bad6ee751df0faf6195b5b
dosleam <9400@holbertonstudents@gmail.com>
2024-06-20 12:13:47 +0000

    C command

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
  *
  * Return: Always 0
  */
@@ -23,12 +24,12 @@ void jack_bauer(void)
 				{
 					for (m2 = '0'; m2 <= '9'; m2++)
 					{
-			_putchar(h1);
-			_putchar(h2);
-			_putchar(':');
-			_putchar(m1);
-			_putchar(m2);
-			_putchar('\n');
+						_putchar(h1);
+						_putchar(h2);
+						_putchar(':');
+						_putchar(m1);
+						_putchar(m2);
+						_putchar('\n');
 					}
 				}
 			}
