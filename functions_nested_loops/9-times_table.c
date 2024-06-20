910a507c4a50af249410efe1c725feb30bf25f53
dosleam <9400@holbertonstudents@gmail.com>
2024-06-20 13:35:23 +0000

    C command

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -11,9 +11,9 @@ void times_table(void)
 	int num1;
 	int num2;
 
-		for (num1 = '0'; num1 <= '9'; num1++)
+		for (num1 = 0; num1 <= 9; num1++)
 		{
-			for (num2 = '0'; num2 <= '9'; num2++)
+			for (num2 = 0; num2 <= 9; num2++)
 			{
 				int a;
 
@@ -23,7 +23,7 @@ void times_table(void)
 					{
 						_putchar(a * '0');
 					}
-					else if (a <= '9')
+					else if (a <= 9)
 					{
 						_putchar(',');
 						_putchar(' ');
