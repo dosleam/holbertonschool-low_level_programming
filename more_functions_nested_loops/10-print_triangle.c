7077463f53066c42362ca12df3840c4371e3d436
dosleam <9400@holbertonstudents.com>
2024-06-24 11:58:24 +0000

    print a triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -16,11 +16,11 @@ void print_triangle(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = 1; j < size; j++)
+			for (j = 1; j < (size - i); j++)
 			{
 				_putchar(' ');
 			}
-			for (t = 0; t <= size; t++)
+			for (t = 0; t <= (size - j); t++)
 			{
 				_putchar('#');
 			}
