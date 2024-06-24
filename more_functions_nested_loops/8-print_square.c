088a799cd4f670eecfbb67173a5698aa3629ab3c
dosleam <9400@holbertonstudents.com>
2024-06-24 10:01:21 +0000

    print a square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - functions that print a square.
+ * @size: size of the square
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; j++)
+		{
+			for (j = 0; i < size; j++)
+			{
+				_putchar('#')
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
