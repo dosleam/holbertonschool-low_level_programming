2b334653ba5c595b3a7a70e5a2158cd9ad5b2831
dosleam <9400@holbertonstudents.com>
2024-06-26 12:57:55 +0000

    concatenates two strings

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two string
+ * @dest: string source
+ * @src: string concatenates
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+        char *d = dest;
+	char *s = src;
+	int i;
+
+        while (*d != '\0')
+        {
+                d++;
+        }
+        for (i = 0; i < n && *s != '\0'; i++)
+	{
+		*d = *s;
+		d++;
+		s++;
+	}
+
+	*d = '\0';
+
+        return (dest);
+}
