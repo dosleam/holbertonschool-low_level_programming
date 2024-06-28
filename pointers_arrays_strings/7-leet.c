43c09937d36dddd8605c636ca4e6e190a045b865
dosleam <9400@holbertonstudents.com>
2024-06-28 10:29:00 +0000

    encodes a string into 1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *leet - function that encodes a string into 1337
+ * @s: string checked
+ *
+ * Return: s
+ */
+char *leet(char *s)
+{
+	int a;
+	int b;
+	char let[] = "aAeEoOtTlL";
+	char num[] = "4433007711";
+
+	for (a = 0; s[a]; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			if (let[b] == s[a])
+				s[a] = num[b];
+		}
+	}
+	return (s);
+}
