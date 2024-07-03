5337512e1a41397b9f9d28f9232a4ccd9678c339
dosleam <9400holbertonstudents.com>
2024-07-03 10:51:13 +0000

    prints a string, followed by a new line.

--- pointers_arrays_strings/0-main.c
+++ pointers_arrays_strings/0-main.c
@@ -1,33 +1,10 @@
+Write a function that prints a string, followed by a new line.
+
+Prototype: void _puts_recursion(char *s);
+FYI: The standard library provides a similar function: puts. Run man puts to learn more.
+
+julien@ubuntu:~/Recursion$ cat 0-main.c
 #include "main.h"
-#include <stdio.h>
-
-/**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-        unsigned int i;
-
-        i = 0;
-        while (i < size)
-        {
-                if (i % 10)
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
-        }
-        printf("\n");
-}
 
 /**
  * main - check the code
@@ -36,11 +13,6 @@ void simple_print_buffer(char *buffer, unsigned int size)
  */
 int main(void)
 {
-    char buffer[98] = {0x00};
-
-    simple_print_buffer(buffer, 98);
-    _memset(buffer, 0x01, 95);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);
+    _puts_recursion("Puts with recursion");
     return (0);
 }
