54f055c24b0ffba2c7268e41061bc52fa1432c23
dosleam <9400@holbertonstudents.com>
2024-07-04 07:42:39 +0000

    update

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -8,11 +8,13 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
+	{
+		putchar(*s);
+	_puts_recursion(s + 1);
+	}
+	else
 	{
 		putchar('\n');
-		return;
 	}
-	putchar(*s);
-	_puts_recursion("s + 1");
 }
