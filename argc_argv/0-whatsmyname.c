0d07475ae5231e8f4982a69c26cfd43b6c425188
dosleam <9400@holbertonstudents.com>
2024-07-05 07:12:49 +0000

    prints its name, followed by a new line.

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,1 +1,16 @@
-i
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: arguments
+ * @argv: arguments
+ * Return: name file
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
