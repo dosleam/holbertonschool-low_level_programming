616a6379498742456ad5c161a63c8092f04864d4
dosleam <9400@holbertonstudents.com>
2024-07-10 11:49:44 +0000

    update

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -8,13 +8,13 @@
 
 void *malloc_checked(unsigned int b)
 {
-	int *ptr;
+	int *p;
 
-	ptr = malloc(b);
+	p = malloc(b);
 
-	if (ptr == NULL)
+	if (p == NULL)
 	{
 		exit(98);
 	}
-	return (ptr);
+	return (p);
 }
