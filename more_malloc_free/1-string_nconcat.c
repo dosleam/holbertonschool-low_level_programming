8b0afc3b8213496c915d43d015c523edd670691f
dosleam <9400@holbertonstudents.com>
2024-07-10 12:04:31 +0000

    update

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -23,32 +23,32 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	}
 
-	l1 = strlen(s1);
-	l2 = strlen(s2);
-
-	if (n >= l2)
+	for (l1 = 0; s1[l1] != '\0'; l1++)
 	{
-		n = l2;
 	}
 
-	result = malloc(l1 + n + 1);
+	for (l2 = 0; s2[l2] != '\0' && n > l2; l2++)
+	{
+	}
+
+	result = malloc((sizeof(char) * (l1 + l2)) + 1);
 
 	if (result == NULL)
 	{
 		return (NULL);
 	}
 
-	for (i = 0; i < l1; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
 		result[i] = s1[i];
 	}
 
-	for (j = 0; j < n; j++)
+	for (j = 0; s2[j] != '\0' && j < n; j++)
 	{
-		result[l1 + j] = s2[j];
+		result[i + j] = s2[j];
 	}
 
-	result[l1 + n] = '\0';
+	result[i + j] = '\0';
 
 	return (result);
 }
