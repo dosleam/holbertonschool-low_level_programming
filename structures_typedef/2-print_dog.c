873b96ec43ae676ad3b29dd48669662310e2a7d8
dosleam <9400@holbertonstudents.com>
2024-07-11 09:46:38 +0000

    prints a struct dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct dog
+ *@d: variables pointers
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == 0)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	
+	if (d->age < 0)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
+
+	if (d == NULL)
+		printf("Owner: (ni)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
