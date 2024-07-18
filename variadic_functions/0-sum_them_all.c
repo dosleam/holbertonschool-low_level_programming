d0a0e9332306cff46993954ec94a0a6680939fab
dosleam <9400@holbertonstudents.com>
2024-07-18 12:08:08 +0000

    returns the sum of all its parameters.

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -20,7 +20,7 @@ int sum_them_all(const unsigned int n, ...)
 		va_start(list, n);
 		for (i = 0; i < n; i++)
 		{
-			result = result + va_arg(list, int);
+			result += va_arg(list, int);
 		}
 		va_end(list);
 		return (result);
