299701e8407d2b6db80ebe8fee4d1167ed7aa0fd
dosleam <9400@holbertonstudents.com>
2024-08-01 11:58:55 +0000

    sets the value of a bit to 0 at a given index

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -11,9 +11,8 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * CHAR_BIT)
 	{
-        return -1;
+		return (-1);
 	}
-	
 	*n &= ~(1UL << index);
-	return 1;
+	return (1);
 }
