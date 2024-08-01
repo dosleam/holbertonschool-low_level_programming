6dee8a821d163b4ae93be45b3e7ad1ea0490b0a4
dosleam <9400@holbertonstudents.com>
2024-08-01 11:12:54 +0000

    returns the value of a bit at a given index

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -4,7 +4,7 @@
  * get_bit - returns the value of a bit at a given index
  * @n: Variable
  * @index: index
- * Return: -1
+ * Return: return value of bit
  */
 
 int get_bit(unsigned long int n, unsigned int index)
