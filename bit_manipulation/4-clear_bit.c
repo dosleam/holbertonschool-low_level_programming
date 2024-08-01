63f0946679dec5ea1927fa2f3b016fb4e25d72f9
dosleam <9400@holbertonstudents.com>
2024-08-01 11:57:08 +0000

    sets the value of a bit to 0 at a given index

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -3,7 +3,7 @@
 /**
  * clear_bit - sets the value of a bit to 0 at a given index
  * @n: variable
- * @index: index
+ * @index: position of a bit
  * Return: 1 if it worked, or -1 if an error occurred
  */
 
