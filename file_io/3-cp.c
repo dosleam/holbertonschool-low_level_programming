1921994eca8708d72a1785733f3526cdd052c68f
dosleam <9400@holbertonstudents.com>
2024-08-02 13:55:43 +0000

    update

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -11,7 +11,8 @@ void copy_file(const char *src, const char *dest)
 	mode_t old_umask = umask(0000);
 
 	from = open(src, O_RDONLY);
-	if (from == -1) {
+	if (from == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
 		exit(98);
 	}
@@ -24,37 +25,28 @@ void copy_file(const char *src, const char *dest)
 	}
 	umask(old_umask);
 	while ((cp = read(from, buff, 1024)) > 0)
-	{
 		if (write(file_to, buff, cp) != cp)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
 			exit(99);
 		}
-	}
 	if (cp == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
 		exit(98);
 	}
-	if (close(from) == -1)
+	if (close(from) == -1 || close(file_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
-		exit(100);
-	}
-	if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
 		exit(100);
 	}
 }
-
 /**
  * main - entry point of the program
- * @argc: number arguments
- * @argv: array strings to the argument
+ * @argc: number of arguments
+ * @argv: array of strings containing the arguments
  * Return: 0
  */
-
 int main(int argc, char **argv)
 {
 	if (argc != 3)
@@ -62,7 +54,6 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	copy_file(argv[1], argv[2]);
-	exit(0);
+	return (0);
 }
