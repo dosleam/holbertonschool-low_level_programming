4edd1e21c6a83232fdd17c2187986275440315e9
dosleam <9400@holbertonstudents.com>
2024-08-02 07:30:30 +0000

    update task 1

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,48 +1,32 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_binary - prints the binary representation of a number
- * @n: variable
+ * print_binary - Prints the binary representation of a number
+ * @n: The number to be printed in binary
  */
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int result = 0;
-	unsigned long int count;
-	unsigned long int i = n;
-
-	if (i == 0)
-		_putchar('0');
-	if (i == 1)
-		_putchar('1');
-
-	while (i > 1)
+	int bit = sizeof(unsigned long int) * 8 - 1;
+	int started = 0;
+	
+	while (bit >= 0)
 	{
-		count = 1;
 
-		while (count * 2 < i)
+		if ((n & (1UL << bit)) != 0)
 		{
-			count = count * 2;
+			putchar('1');
+			started = 1;
 		}
-
-		result = result + count;
-
-		if (result <= n && result * 2 != n)
-			_putchar('1');
-
-		else if (result * 2 == n)
+		else if (started)
 		{
-			_putchar('1');
-			result = result * 2;
-			count = count * 2;
+			putchar('0');
 		}
-
-		else
-		{
-			result = result - count;
-			_putchar('0');
-		}
-
-		i = count;
+		bit--;
+	}
+	if (!started)
+	{
+		putchar('0');
 	}
 }
