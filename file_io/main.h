00741fab3d2d1e439df14b6c0692798d01c387f2
dosleam <9400@holbertonstudents.com>
2024-08-02 13:17:15 +0000

    update the code

--- file_io/main.h
+++ file_io/main.h
@@ -7,8 +7,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <elf.h>
-#include <errno.h>
+#include <sys/uio.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
