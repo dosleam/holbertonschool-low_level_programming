1de6a9c5e27239d92bb0f5ad02e8ac9724b3be71
dosleam <9400@holbertonstudents.com>
2024-08-02 09:59:09 +0000

    create a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - creates a file
  * @filename: path to the file
- * @text_content: the content
+ * @text_content: the text content
  * Return: 1 on success, -1 on failure
  */
 
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	int len = 0;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
