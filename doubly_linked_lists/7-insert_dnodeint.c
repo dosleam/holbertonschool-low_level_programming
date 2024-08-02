6fe26a3b66ba43a55f0caed9e76c237b7c9f150a
dosleam <9400@holbertonstudents.com>
2024-08-02 07:08:29 +0000

    update task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,21 +14,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int cp = 0;
 
 	newnode = malloc(sizeof(dlistint_t));
-
 	if (newnode == NULL)
 		return (NULL);
 
 	newnode->n = n;
 	newnode->next = NULL;
 	newnode->prev = NULL;
-
 	if (idx == 0)
 	{
 		newnode->next = *h;
-
 		if (*h != NULL)
 			(*h)->prev = newnode;
-
 		*h = newnode;
 	}
 	else
@@ -38,24 +34,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			current = current->next;
 			cp++;
 		}
-
 		if (current != NULL)
 		{
 			newnode->next = current->next;
 			newnode->prev = current;
-
 			if (current->next != NULL)
 				current->next->prev = newnode;
-
 			current->next = newnode;
 		}
-
 		else
 		{
 			free(newnode);
 			return (NULL);
 		}
 	}
-	
 	return (newnode);
 }
