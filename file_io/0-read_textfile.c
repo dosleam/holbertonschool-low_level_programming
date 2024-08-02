e2ba7a398efa1753e01d0aacf79a00bbb4587227
dosleam <9400@holbertonstudents.com>
2024-08-02 08:36:23 +0000

    reads a text file and prints it to the POSIX standard output.

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: path to the file
+ * @letters: the numbers of letters
+ * Return: returns the actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytesRead, bytesWritten;
+	char *buffer;
+	
+	if (filename == NULL)
+		return (0);
+	/* Ouverture du fichier */
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	/* Allocation de la memoire pour le tampon */
+	buffer = (char *)malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	/* Lecture du fichier */
+	bytesRead = read(fd, buffer, letters);
+	if (bytesRead == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	/* Ecris sur la sortie standard */
+	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+	if (bytesWritten == -1 || bytesWritten != bytesRead)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	/* Nettoyage */
+	free(buffer);
+	close(fd);
+	
+	return (bytesWritten);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -1,6 +1,15 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <elf.h>
 
-int _putchar(char c)
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
