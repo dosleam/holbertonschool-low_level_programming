61eea0cf2d2b9c31f5d432a10dd188f799c8b0e7
dosleam <9400@holbertonstudents.com>
2024-08-02 13:46:55 +0000

    update

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,24 +1,20 @@
 #include "main.h"
-
 /**
  * copy_file - copies the content of a file to another file
  * @src: pointer file
  * @dest: pointer file destination
  */
-
 void copy_file(const char *src, const char *dest)
 {
 	int from, file_to, cp;
 	char buff[1024];
-	mode_t old_umask;
+	mode_t old_umask = umask(0000);
 
 	from = open(src, O_RDONLY);
-	if (from == -1)
-	{
+	if (from == -1) {
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
 		exit(98);
 	}
-	old_umask = umask(0000);
 	file_to = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
